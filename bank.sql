a310ee0ec02ffd4b46de5f0ef3dd1806bcd42154
CHILUKURI SRI HARSHA <sharshach@gmail.com>
2020-11-26 19:33:51 +0530

    Removed Errors Highlighted SYNTAX well

--- bank.sql
+++ bank.sql
@@ -1,106 +1,113 @@
 CREATE DATABASE bank;
 USE bank;
-create table account
-(account_number varchar(15) not null unique,
-branch_name varchar(15) not null,
-balance number not null,
-primary key(account_number));
-create table branch
-(branch_name varchar(15) not null unique,
-branch_city varchar(15) not null,
-assets number not null,
-primary key(branch_name));
 
-create table customer
-(customer_name varchar(15) not null unique,
-customer_street varchar(12) not null,
-customer_city varchar(15) not null,
-primary key(customer_name));
+CREATE TABLE account(
+    account_number VARCHAR(15) NOT NULL,
+    branch_name VARCHAR(15) NOT NULL,
+    balance INT NOT NULL,
+    PRIMARY KEY(account_number)
+);
 
-create table loan
-(loan_number varchar(15) not null unique,
-branch_name varchar(15) not null,
-amount number not null,
-primary key(loan_number));
+CREATE TABLE branch(
+    branch_name VARCHAR(15) NOT NULL,
+    branch_city VARCHAR(15) NOT NULL,
+    assets INT NOT NULL,
+    PRIMARY KEY(branch_name)
+);
 
-create table depositor
-(customer_name varchar(15) not null,
-account_number varchar(15) not null,
-primary key(customer_name, account_number),
-foreign key(account_number) references account(account_number),
-foreign key(customer_name) references customer(customer_name));
+CREATE TABLE customer(
+    customer_name VARCHAR(15) NOT NULL,
+    customer_street VARCHAR(12) NOT NULL,
+    customer_city VARCHAR(15) NOT NULL,
+    PRIMARY KEY(customer_name)
+);
 
-create table borrower
-(customer_name varchar(15) not null,
-loan_number varchar(15) not null,
-primary key(customer_name, loan_number),
-foreign key(customer_name) references customer(customer_name),
-foreign key(loan_number) references loan(loan_number));
+CREATE TABLE loan(
+    loan_number VARCHAR(15) NOT NULL,
+    branch_name VARCHAR(15) NOT NULL,
+    amount INT NOT NULL,
+    PRIMARY KEY(loan_number)
+);
 
-insert into customer values (‘Jones’, ‘Main’, ‘Harrison’);
-insert into customer values (‘Smith’, ‘Main’, ‘Rye’);
-insert into customer values (‘Hayes’, ‘Main’, ‘Harrison’);
-insert into customer values (‘Curry’, ‘North’, ‘Rye’);
-insert into customer values (‘Lindsay’, ‘Park’, ‘Pittsfield’);
-insert into customer values (‘Turner’, ‘Putnam’, ‘Stamford’);
-insert into customer values (‘Williams’, ‘Nassau’, ‘Princeton’);
-insert into customer values (‘Adams’, ‘Spring’, ‘Pittsfield’);
-insert into customer values (‘Johnson’, ‘Alma’, ‘Palo Alto’);
-insert into customer values (‘Glenn’, ‘Sand Hill’, ‘Woodside’);
-insert into customer values (‘Brooks’, ‘Senator’, ‘Brooklyn’);
-insert into customer values (‘Green’, ‘Walnut’, ‘Stamford’);
-insert into customer values (‘Jackson’, ‘University’, ‘Salt Lake’);
-insert into customer values (‘Majeris’, ‘First’, ‘Rye’);
-insert into customer values (‘McBride’, ‘Safety’, ‘Rye’);
+CREATE TABLE depositor(
+    customer_name VARCHAR(15) NOT NULL,
+    account_number VARCHAR(15) NOT NULL,
+    PRIMARY KEY(customer_name, account_number),
+    FOREIGN KEY(account_number) REFERENCES account(account_number),
+    FOREIGN KEY(customer_name) REFERENCES customer(customer_name)
+);
 
-insert into branch values (‘Downtown’, ‘Brooklyn’, 900000);
-insert into branch values (‘Redwood’, ‘Palo Alto’, 2100000);
-insert into branch values (‘Perryridge’, ‘Horseneck’, 1700000);
-insert into branch values (‘Mianus’, ‘Horseneck’, 400200);
-insert into branch values (‘Round Hill’, ‘Horseneck’, 8000000);
-insert into branch values (‘Pownal’, ‘Bennington’, 400000);
-insert into branch values (‘North Town’, ‘Rye’, 3700000);
-insert into branch values (‘Brighton’, ‘Brooklyn’, 7000000);
-insert into branch values (‘Central’, ‘Rye’, 400280);
+CREATE TABLE borrower(
+    customer_name VARCHAR(15) NOT NULL,
+    loan_number VARCHAR(15) NOT NULL,
+    PRIMARY KEY(customer_name, loan_number),
+    FOREIGN KEY(customer_name) REFERENCES customer(customer_name),
+    FOREIGN KEY(loan_number) REFERENCES loan(loan_number)
+);
+INSERT INTO customer VALUES ('Jones', 'Main', 'Harrison');
+INSERT INTO customer VALUES ('Smith', 'Main', 'Rye');
+INSERT INTO customer VALUES ('Hayes', 'Main', 'Harrison');
+INSERT INTO customer VALUES ('Curry', 'North', 'Rye');
+INSERT INTO customer VALUES ('Lindsay', 'Park', 'Pittsfield');
+INSERT INTO customer VALUES ('Turner', 'Putnam', 'Stamford');
+INSERT INTO customer VALUES ('Williams', 'Nassau', 'Princeton');
+INSERT INTO customer VALUES ('Adams', 'Spring', 'Pittsfield');
+INSERT INTO customer VALUES ('Johnson', 'Alma', 'Palo Alto');
+INSERT INTO customer VALUES ('Glenn', 'Sand Hill', 'Woodside');
+INSERT INTO customer VALUES ('Brooks', 'Senator', 'Brooklyn');
+INSERT INTO customer VALUES ('Green', 'Walnut', 'Stamford');
+INSERT INTO customer VALUES ('Jackson', 'University', 'Salt Lake');
+INSERT INTO customer VALUES ('Majeris', 'First', 'Rye');
+INSERT INTO customer VALUES ('McBride', 'Safety', 'Rye');
 
-insert into account values (‘A-101’, ‘Downtown’, 500);
-insert into account values (‘A-215’, ‘Mianus’, 700);
-insert into account values (‘A-102’, ‘Perryridge’, 400);
-insert into account values (‘A-305’, ‘Round Hill’, 350);
-insert into account values (‘A-201’, ‘Perryridge’, 900);
-insert into account values (‘A-222’, ‘Redwood’, 700);
-insert into account values (‘A-217’, ‘Brighton’, 750);
-insert into account values (‘A-333’, ‘Central’, 850);
-insert into account values (‘A-444’, ‘North Town’, 625);
+INSERT INTO branch VALUES ('Downtown', 'Brooklyn', 900000);
+INSERT INTO branch VALUES ('Redwood', 'Palo Alto', 2100000);
+INSERT INTO branch VALUES ('Perryridge', 'Horseneck', 1700000);
+INSERT INTO branch VALUES ('Mianus', 'Horseneck', 400200);
+INSERT INTO branch VALUES ('Round Hill', 'Horseneck', 8000000);
+INSERT INTO branch VALUES ('Pownal', 'Bennington', 400000);
+INSERT INTO branch VALUES ('North Town', 'Rye', 3700000);
+INSERT INTO branch VALUES ('Brighton', 'Brooklyn', 7000000);
+INSERT INTO branch VALUES ('Central', 'Rye', 400280);
 
-insert into depositor values (‘Johnson’,’A-101′);
-insert into depositor values (‘Smith’, ‘A-215’);
-insert into depositor values (‘Hayes’, ‘A-102’);
-insert into depositor values (‘Hayes’, ‘A-101’);
-insert into depositor values (‘Turner’, ‘A-305’);
-insert into depositor values (‘Johnson’,’A-201′);
-insert into depositor values (‘Jones’, ‘A-217’);
-insert into depositor values (‘Lindsay’,’A-222′);
-insert into depositor values (‘Majeris’,’A-333′);
-insert into depositor values (‘Smith’, ‘A-444’);
+INSERT INTO account VALUES ('A-101', 'Downtown', 500);
+INSERT INTO account VALUES ('A-215', 'Mianus', 700);
+INSERT INTO account VALUES ('A-102', 'Perryridge', 400);
+INSERT INTO account VALUES ('A-305', 'Round Hill', 350);
+INSERT INTO account VALUES ('A-201', 'Perryridge', 900);
+INSERT INTO account VALUES ('A-222', 'Redwood', 700);
+INSERT INTO account VALUES ('A-217', 'Brighton', 750);
+INSERT INTO account VALUES ('A-333', 'Central', 850);
+INSERT INTO account VALUES ('A-444', 'North Town', 625);
 
-insert into loan values (‘L-17’, ‘Downtown’, 1000);
-insert into loan values (‘L-23’, ‘Redwood’, 2000);
-insert into loan values (‘L-15’, ‘Perryridge’, 1500);
-insert into loan values (‘L-14’, ‘Downtown’, 1500);
-insert into loan values (‘L-93’, ‘Mianus’, 500);
-insert into loan values (‘L-11’, ‘Round Hill’, 900);
-insert into loan values (‘L-16’, ‘Perryridge’, 1300);
-insert into loan values (‘L-20’, ‘North Town’, 7500);
-insert into loan values (‘L-21’, ‘Central’, 570);
+INSERT INTO depositor VALUES ('Johnson','A-101');
+INSERT INTO depositor VALUES ('Smith', 'A-215');
+INSERT INTO depositor VALUES ('Hayes', 'A-102');
+INSERT INTO depositor VALUES ('Hayes', 'A-101');
+INSERT INTO depositor VALUES ('Turner', 'A-305');
+INSERT INTO depositor VALUES ('Johnson','A-201');
+INSERT INTO depositor VALUES ('Jones', 'A-217');
+INSERT INTO depositor VALUES ('Lindsay','A-222');
+INSERT INTO depositor VALUES ('Majeris','A-333');
+INSERT INTO depositor VALUES ('Smith', 'A-444');
 
-insert into borrower values (‘Jones’, ‘L-17’);
-insert into borrower values (‘Smith’, ‘L-23’);
-insert into borrower values (‘Hayes’, ‘L-15’);
-insert into borrower values (‘Jackson’, ‘L-14’);
-insert into borrower values (‘Curry’, ‘L-93’);
-insert into borrower values (‘Smith’, ‘L-11’);
-insert into borrower values (‘Williams’,’L-17′);
-insert into borrower values (‘Adams’, ‘L-16’);
-insert into borrower values (‘McBride’, ‘L-20’);
-insert into borrower values (‘Smith’, ‘L-21’);
+INSERT INTO loan VALUES ('L-17', 'Downtown', 1000);
+INSERT INTO loan VALUES ('L-23', 'Redwood', 2000);
+INSERT INTO loan VALUES ('L-15', 'Perryridge', 1500);
+INSERT INTO loan VALUES ('L-14', 'Downtown', 1500);
+INSERT INTO loan VALUES ('L-93', 'Mianus', 500);
+INSERT INTO loan VALUES ('L-11', 'Round Hill', 900);
+INSERT INTO loan VALUES ('L-16', 'Perryridge', 1300);
+INSERT INTO loan VALUES ('L-20', 'North Town', 7500);
+INSERT INTO loan VALUES ('L-21', 'Central', 570);
+
+INSERT INTO borrower VALUES ('Jones', 'L-17');
+INSERT INTO borrower VALUES ('Smith', 'L-23');
+INSERT INTO borrower VALUES ('Hayes', 'L-15');
+INSERT INTO borrower VALUES ('Jackson', 'L-14');
+INSERT INTO borrower VALUES ('Curry', 'L-93');
+INSERT INTO borrower VALUES ('Smith', 'L-11');
+INSERT INTO borrower VALUES ('Williams','L-17');
+INSERT INTO borrower VALUES ('Adams', 'L-16');
+INSERT INTO borrower VALUES ('McBride', 'L-20');
+INSERT INTO borrower VALUES ('Smith', 'L-21');
